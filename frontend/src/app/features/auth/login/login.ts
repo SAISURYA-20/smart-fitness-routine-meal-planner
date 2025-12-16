28ddf8428c79326cae5c5bb323fdd1fa371712ae
SAISURYA-20 <suryakaruturi20@gmail.com>
2025-12-16 13:36:36 +0530

    feat(auth): login route renders successfully

---
+++ frontend/src/app/features/auth/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.scss']
+})
+export class LoginComponent {
+}
---
+++ frontend/src/app/features/auth/register/register.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './register.html',
+  styleUrls: ['./register.scss']
+})
+export class RegisterComponent {
+}
